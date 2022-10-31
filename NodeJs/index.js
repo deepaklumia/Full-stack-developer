bb0f6546e0db5d43ff52adca28340e62f91584fe
deepaklumia <deepakkumar12071998@gmail.com>
2022-10-31 08:42:07 +0530

    run resver and connect to monggose

--- NodeJs/index.js
+++ NodeJs/index.js
@@ -1,1 +1,25 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const { default: mongoose } = require("mongoose");
+const router = require("./router/router");
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+mongoose
+    .connect(
+        "mongodb+srv://deepak98:deepaklumia@book.4bu11tl.mongodb.net/test",
+        { useNewUrlParser: true }
+    )
+    .then(() => {
+        console.log("mongodb is connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+    
+app.use("/", router);
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Express app running on port " + (process.env.PORT || 3000));
+});
